e98903799a114c2595acd6e99361a0afdb29b114
karalkal <karalkal@yahoo.co.uk>
2024-02-04 04:00:29 +0000

    card cover image styled

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,14 +4,20 @@ import { Link } from "react-router-dom"
 
 export default function Card({ album }) {
     return (
-        <Link to={`/KUR/${album.id.toString()}`} className={styles.cardContainer}>
-            <p> {album.id} </p>
-            <p> {album.name} </p>
-            <p> {album.band_name} </p>
-            <p> {album.colour} </p>
-            <p> {album.cover} </p>
-            <p> {album.id} </p>
-            <p> {album.price} </p>
-            <p> {album.release_year} </p>
-        </Link>)
+        <Link to={`/KUR/${album.id.toString()}`} className={styles.card}>
+            <img
+                className={styles.square}
+                src={album.cover}
+                alt={`${album.name}by${album.band_name}`}
+            />
+            <div className={styles.cardText}>
+                <p> {album.id} </p>
+                <p> {album.name} </p>
+                <p> {album.band_name} </p>
+                <p> {album.colour} </p>
+                <p> {album.price} </p>
+                <p> {album.release_year} </p>
+            </div>
+        </Link>
+    )
 }
