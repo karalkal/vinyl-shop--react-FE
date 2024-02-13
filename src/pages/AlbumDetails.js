8114a56ac6177d158929accb635b4cf3a60835ba
karalkal <karalkal@yahoo.co.uk>
2024-02-13 23:36:22 +0000

    styling of AlbumDetails started

--- src/pages/AlbumDetails.js
+++ src/pages/AlbumDetails.js
@@ -5,14 +5,15 @@ import { useLoaderData } from "react-router-dom";
 
 const AlbumDetails = () => {
     const albumData = useLoaderData();
-    console.log(albumData);
-
+    // name, band_name, label_name, cover, release_year, // => MUST HAVE
+    // colour, summary, duration, format, quantity, price  // => OPTIONAL
     if (!albumData) {
         console.log("Nada!")
         return
     }
     return (
         <div className={styles.detailsContainer}>
+            {/* all albums will have an image in DB but if not in correct format, i.e. not an URL, display message */}
             {(albumData.cover).includes("http")
                 ?
                 <img
@@ -25,18 +26,21 @@ const AlbumDetails = () => {
                 </div>
             }
             <div className={styles.textPrimary}>
-                <p>cat.ID: {albumData.id}</p>
-                <h1 className={styles.albumTitle}>{albumData.name}</h1>
-                <p>By<span className={styles.bandName}>&nbsp;{albumData.band_name}</span></p>
-                <p>Released<span className={styles.releaseYear}>&nbsp;{albumData.release_year}</span></p>
-                <p>Label<span className={styles.labelName}> {albumData.label_name}</span></p>
-                <p>Colour: {albumData.color}</p>
-                <p>Duration: {albumData.duration}</p>
-                <p>Format:  {albumData.format}</p>
-                <p>Price: {albumData.price}</p>
-                <p>Genres: {(albumData.genre_array).join("; ")}</p>
-                <p>Summary: {albumData.summary}</p>
-                <p>Quantity available: {albumData.quantity}</p>
+                <p className={styles.itemId}>item ID: {albumData.id}</p>
+                <p className={styles.albumTitle}>{albumData.name}</p>
+                <p >By<span className={styles.bandName}>&nbsp;{albumData.band_name}</span></p>
+                <div className={styles.albumData}>
+                    <p className={styles.albumDataLabel}>Released:<span>&nbsp;{albumData.release_year}</span></p>
+                    <p className={styles.albumDataLabel}>Label:<span>&nbsp;{albumData.label_name}</span></p>
+                    <p className={styles.albumDataLabel}>Colour:<span>&nbsp;{albumData.color || 'N.A.'}</span></p>
+                    <p className={styles.albumDataLabel}>Duration:<span>&nbsp;{albumData.duration || 'N.A.'}</span></p>
+                    <p className={styles.albumDataLabel}>Format:<span>&nbsp;{albumData.format || 'N.A.'}</span></p>
+                    <p className={styles.albumDataLabel}>Price:<span>&nbsp;{albumData.price || 'N.A.'}</span></p>
+                </div>
+                <p>Genres: {(albumData.genre_array).join("; ") || 'N.A.'}</p>
+                <p>Quantity available: {albumData.quantity || 'N.A.'}</p>
+
+                <p>Summary: {albumData.summary || 'N.A.'}</p>
             </div>
             <Button style={{ gridArea: 'addBtn', width: '100%', bottom: '0', position: 'absolute' }}>Add to Cart</Button>
         </div>
