b00e6ef776411d311d0d0f5ea1f0558afe4de5eb
karalkal <karalkal@yahoo.co.uk>
2024-02-18 00:47:48 +0000

    adding items to cart enabled

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,49 +3,51 @@ import React from 'react';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 import CartContext from '../context/CartContextProvider';
-// import CartItem from './CartItem'
+import { CartItem } from './CartItem'
 
 export const Cart = (props) => {
 
     const ctx = React.useContext(CartContext);
     console.log(ctx)
-    /*
+    // utilize function addItem in context by passing object with same id and amount of 1
+    function incrementItemHandler(item) {
+        const incrementedMeal = {
+            ...item,
+            amount: 1
+        }
+        ctx.addItem(incrementedMeal)
+    }
 
 
-// utilize function addItem in context by passing object with same id and amount of 1
-function incrementItemHandler(item) {
-const incrementedMeal = {
-...item,
-amount: 1
-}
-ctx.addItem(incrementedMeal)
-}
+    function decrementItemHandler(itemID) {
+        ctx.decrementItem(itemID)
+    }
 
+    function removeItemHandler(itemID) {
+        ctx.removeItem(itemID)
+    }
 
-
-const cartItems = (
-<ul className={classes['cart-items']}>
-{ctx.items.map((item) => (
-  <CartItem
-    key={item.id}
-    // name={item.name}
-    // price={item.price}
-    // amount={item.amount}
-    item={item}       // pass whole obj as prop so we can increment/decrement amount easier from child
-    onIncrement={incrementItemHandler}
-    onDecrement={decrementItemHandler}
-    onRemove={removeItemHandler}
-  />
- <h1>item.name</h1>
-))}
-</ul>
-);
-    */
+    const cartItems = (
+        <ul className={classes['cart-items']}>
+            {ctx.items.map((item) => (
+                <CartItem
+                    key={item.id}
+                    // name={item.name}
+                    // price={item.price}
+                    // amount={item.amount}
+                    item={item}       // pass whole obj as prop so we can increment/decrement amount easier from child
+                    onIncrement={incrementItemHandler}
+                    onDecrement={decrementItemHandler}
+                    onRemove={removeItemHandler}
+                />
+            ))}
+        </ul>
+    );
 
 
     return (
         <Modal onHideCart={props.onHideCart}>
-            <h1>KUR</h1>
+            <h1>{cartItems}</h1>
         </Modal>
     );
 };
---
+++ src/components/CartItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import classes from './CartItem.module.css';
+
+
+export const CartItem = (props) => {
+    console.log(props);
+    const { name, band_name, colour, cover, format, id, price, release_year } = props.item
+
+    // in this version I pass whole item obj as props so we can manipulate amount from this component
+    return (
+        <li className={classes['cart-item']}>
+            <div>
+                <h2>{name}</h2>
+                <h2>{band_name}</h2>
+                <div className={classes.summary}>
+                    {/* <span className={classes.price}>${props.item.price.toFixed(2)}</span> */}
+                    <span className={classes.amount}>x {props.item.amount}</span>
+                </div>
+            </div>
+            <div className={classes.actions}>
+                <button onClick={() => props.onDecrement(props.item.id)}>−</button>
+                <button onClick={() => props.onIncrement(props.item)}>+</button>
+                <button onClick={() => props.onRemove(props.item.id)}><i className="fa-solid fa-trash"></i></button>
+            </div>
+        </li>
+    );
+};
