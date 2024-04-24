09a816981abac33cbe330bf273f3bc948c369114
karalkal <karalkal@yahoo.co.uk>
2024-04-24 23:26:10 +0100

    added ErrorModal

--- src/modals/ErrorModal.js
+++ src/modals/ErrorModal.js
@@ -1,18 +1,34 @@
-import styles from './ErrorModal.module.css'
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 
-export default function ErrorModal({ handleClose, show, errorMsg }) {
-    const showHideClassName = show
-        ? `${styles.modal} ${styles.displayBlock}`
-        : `${styles.modal} ${styles.displayNone}`;
+import styles from './ErrorModal.module.css';
 
-    return (
-        <div className={showHideClassName}>
-            <section className={styles.modalMain}>
-                <p>{errorMsg}</p>
-                <button id={styles.modalBtn} type="button" onClick={handleClose}>
-                    Close
-                </button>
-            </section>
-        </div>
-    );
+import { Backdrop } from '../layouts/Modal';
+
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.errorModal}>
+      {props.children}
+    </div>
+  );
 };
+
+// created beforehand <div id="overlays"></div> in index.html
+// where modal will be rendered
+const portalElement = document.getElementById('overlays');
+
+const ErrorModal = (props) => {
+
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default ErrorModal;
--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
-import styles from "./Errors.module.css"
+import styles from "./Errors.module.css";
+import ErrorModal from "../modals/ErrorModal";
 
 
 export default function PageNotFound() {
     return (
-        <div className={styles.errorDiv}>
+        <ErrorModal>
             <h1 className={styles.errorTitle}>Page not found.</h1>
             <h2 className={styles.errorSubtitle}>
                 <Link to="/"
                     className={styles.errorSubtitle}>Return to Homepage</Link></h2>
-        </div>
+        </ErrorModal>
+
     );
 };
 
